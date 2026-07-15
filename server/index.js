16909b2d1548a4d6cf057e54cb91fbba593e6e0e
Lakshita Israni <lakshitaisrani09@gmail.com>
2026-07-15 13:29:10 +0530

    fix CORS preflight check logic for production

--- server/index.js
+++ server/index.js
@@ -14,15 +14,12 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-const allowedOrigins = process.env.FRONTEND_URL ? [process.env.FRONTEND_URL] : ['http://localhost:5173'];
+const frontendUrl = process.env.FRONTEND_URL;
+const cleanFrontendUrl = frontendUrl ? frontendUrl.replace(/\/$/, '') : null;
+const allowedOrigins = cleanFrontendUrl ? [cleanFrontendUrl, 'http://localhost:5173'] : ['http://localhost:5173'];
+
 app.use(cors({
-    origin: function(origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1 || process.env.NODE_ENV === 'development') {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: allowedOrigins,
     credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
